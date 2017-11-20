8d9672eed72ec57a6d5de0aad4820aebdefc5df9
Pietro Martini <pmartini@bermangrp.com>
2017-11-20 17:45:25 -0500

    update func name

--- find_even_index.js
+++ find_even_index.js
@@ -1,6 +1,3 @@
-const getSum = arr => arr.reduce((accum, el) => {
-  accum += el;
-  return accum;
-}, 0);
+const sumArrayVals = arr => arr.reduce(( acc, cur ) => acc + cur, 0);
 
-const findEvenIndex = arr => arr.findIndex((el, i) => getSum(arr.slice(0, i)) === getSum(arr.slice(i + 1)));
+const findEvenIndex = arr => arr.findIndex((el, i) => sumArrayVals(arr.slice(0, i)) === sumArrayVals(arr.slice(i + 1)));
